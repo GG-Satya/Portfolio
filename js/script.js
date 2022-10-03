10ac5a0ef3cdd181baebab53ff7d62f2e65ccc51
Satyaprasanna Dash <111240668+GG-Satya@users.noreply.github.com>
2022-10-04 00:01:22 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ================== Typing Animation ==================
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Java Developer","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
